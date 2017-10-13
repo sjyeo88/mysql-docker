e4d312e793da6f5ecfb367dffee4c35118013a6b
sjyeo88 <sjyeo88@gmail.com>
2017-10-14 02:51:16 +0900

    Apply Docker

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,5 @@
 #Build Docker Image
-docker build --tag mysql . 
+sudo docker build --tag mysql . 
 
 #Create Local Volume
 if ! [ -d /opt/mysql ]; then
@@ -7,4 +7,4 @@ sudo mkdir /opt/mysql
 fi
 
 ##Run Container
-docker run -d --name db-vol --volume /opt/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=!dutkak3 -p 3306:3306  mysql
+sudo docker run -d --name db-vol --volume /opt/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=!dutkak3 -p 3306:3306  mysql
